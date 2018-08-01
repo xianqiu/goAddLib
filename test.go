d0847dd72ac5e8db1b6d1c2b36a3d5bd52582c0e
NullGo <x.qiu@qq.com>
2018-08-01 21:25:29 +0800

    regoranize files

---
+++ test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"addlib/addlib"
+	"fmt"
+	"time"
+)
+
+func test() {
+	t1 := time.Now()
+	for i:=0; i < 100; i++ {
+		for _, p := range addlib.Provinces() {
+			for _, c := range addlib.Cities(p) {
+				for _, d := range addlib.Districts(c) {
+					_ = d
+				}
+			}
+		}
+	}
+	t2 := time.Since(t1)
+	fmt.Println(t2)
+}
+
+func main() {
+	test()
+}
