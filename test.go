351f31e8f32c6178f6ad741d5ef56afcff8c3cc6
xqiu <x.qiu@qq.com>
2018-08-08 09:00:42 +0800

    change file name

--- test.go
+++ test.go
@@ -9,13 +9,15 @@ import (
 func test() {
 	t1 := time.Now()
 	for i:=0; i < 100; i++ {
-		for _, p := range addlib.Provinces() {
-			for _, c := range addlib.Cities(p) {
-				for _, d := range addlib.Districts(c) {
-					_ = d
+		go func () {
+			for _, p := range addlib.Provinces() {
+				for _, c := range addlib.Cities(p) {
+					for _, d := range addlib.Districts(c) {
+						_ = d
+					}
 				}
 			}
-		}
+		}()
 	}
 	t2 := time.Since(t1)
 	fmt.Println(t2)
